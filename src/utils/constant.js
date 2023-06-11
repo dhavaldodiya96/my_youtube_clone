279f5ba06734d5d97f3b2606f83e2743fd4a83f8
Dhaval Dodiya <dhavaldodiya96@gmail.com>
2023-06-11 17:36:04 +0530

    Chenged base URL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@
 const GOOGLE_API = "AIzaSyCrCo8WXpFJ3Ma0uxMD1Tp8iJlyghtV8I4";
 
 
-export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=us&key="+
 GOOGLE_API
 
 export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
